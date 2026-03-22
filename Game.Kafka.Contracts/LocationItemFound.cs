a10d5fe1a4d0f356309b95ca3f68d3f8e30ca1c0
Rona <nikakyl2015@gmail.com>
2026-03-22 12:20:31 +0300

    item found

--- Game.Kafka.Contracts/LocationItemFound.cs
+++ Game.Kafka.Contracts/LocationItemFound.cs
@@ -3,6 +3,7 @@
     public class LocationItemFound
     {
         public Guid LocationId { get; set; }
+        public Guid ItemId { get; set; }
         public string Location { get; set; } = string.Empty;
     }
 }
--- Game.Kafka.Contracts/TypeItemFound.cs
+++ Game.Kafka.Contracts/TypeItemFound.cs
@@ -3,6 +3,7 @@
     public class TypeItemFound
     {
         public Guid TypeId { get; set; }
+        public Guid ItemId { get; set; }
         public string Type { get; set; } = string.Empty;
     }
 }
