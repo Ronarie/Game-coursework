1f138d4fb84c3a80d76e929e94f39e7eb50be3c5
Rona <nikakyl2015@gmail.com>
2026-03-22 12:11:00 +0300

    kafka message

--- Game.Wiki/KafkaClient.cs
+++ Game.Wiki/KafkaClient.cs
@@ -9,7 +9,20 @@ namespace Game.Wiki
         private readonly string _bootstrapServers = "localhost:9092";
         private readonly string _topic = "game.status";
 
-        public async Task PublishProgressMessageAsync(ProgressMessage message)
+        public async Task LocationStatusMessageAsync(LocationItemFound message)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = _bootstrapServers
+            };
+
+            using var producer = new ProducerBuilder<Null, string>(config).Build();
+            var json = JsonSerializer.Serialize(message);
+
+            var result = await producer.ProduceAsync(_topic, new Message<Null, string> { Value = json });
+            Console.WriteLine($"Kafka: отправлено сообщение в {_topic} (offset {result.Offset})");
+        }
+        public async Task TypeStatusMessageAsync(TypeItemFound message)
         {
             var config = new ProducerConfig
             {
